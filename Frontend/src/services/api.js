cb7c48e4270d58041f1d343e2265b160438df984
uzain123 <115154318+uzain123@users.noreply.github.com>
2025-09-29 22:52:32 +0500

    start and demo login page
    login page to undestand the flow

---
+++ Frontend/src/services/api.js
@@ -0,0 +1,17 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:5000/api", // backend URL
+//   headers: { "Content-Type": "application/json" },
+// });
+
+// // attach token if exists
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers["token"] = token; // using 'token' header as per your backend
+//   }
+//   return config;
+// });
+
+// export default api;
---
+++ Frontend/src/services/authAPI.js
@@ -0,0 +1,10 @@
+// import api from "./api";
+
+// export async function loginUser(credentials) {
+//   try {
+//     const res = await api.post("/auth/login", credentials);
+//     return res.data; // { token, user }
+//   } catch (err) {
+//     throw new Error(err.response?.data?.message || "Login failed");
+//   }
+// }
